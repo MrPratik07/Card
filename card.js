7f965434757a0602c9074b496a6fd3936188196a
pratik <pratikgaikwad551@gmail.com>
2021-05-16 08:41:51 +0530

    first commit

---
+++ card.js
@@ -0,0 +1,86 @@
+const postData = [
+    {
+        author: "Raunak Srivastava",
+        title: "Hello there,This is Post!",
+        summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        author: "Nakul Gopal",
+        title: "Hello there,This is Post!",
+        summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        author: "Shubham Soni",
+        title: "Hello there,This is Post!",
+        summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        author: "Abhishek Kumar",
+        title: "Hello there,This is Post!",
+        summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        author: "Sameer Soni",
+        title: "Hello there,This is Post!",
+        summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+    {
+        author: "Manikantha S",
+        title: "Hello there,This is Post!",
+        summary: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    },
+
+]
+
+let postDataState;
+let lastDeleted = [];
+
+
+const addPostContent = (data=postData) => {
+    const postContainer = document.getElementsByClassName("main-container")[0]
+    data.forEach((post, index) => {
+        const { title,author,summary} = post // Object destructuring
+        const cardDiv = document.createElement("div")
+        cardDiv.className = "card"
+        cardDiv.innerHTML = `<div class="author-container">
+                                <h5>${author}</h5>
+                            </div>
+                            <div class="post-container">
+                                <h3>${title} ${index + 1}</h3>
+                                <p>${summary}</p>
+                                <i class="fa fa-trash trash-icon" aria-hidden="true" onclick="deleteClickHandler(${index})" ></i>
+                                <i class="fa fa-ellipsis-h elipsis-icon" aria-hidden="true"></i>
+                            </div>`
+        postContainer.appendChild(cardDiv)
+    })
+    postDataState = data
+}
+
+const removeAllElements = (className) => {
+    const element=document.getElementsByClassName(className)[0];
+    element.innerHTML="";
+
+}
+
+const deleteClickHandler =(index) =>{
+    lastDeleted.push(postData.filter((item,i)=> i===index))
+    const newPostData=postDataState.filter((item,i)=> i!==index)
+    removeAllElements("main-container")
+    addPostContent(newPostData)
+
+}
+
+const getAllDataClickHandler = () =>{
+    removeAllElements("main-container")
+    addPostContent()
+}
+
+const undoClickHandler = () =>{
+    if(lastDeleted.length === 0) return;
+    const newPostData = [...postDataState]
+    newPostData.push(lastDeleted.pop()[0])
+    removeAllElements("main-container")
+    addPostContent(newPostData)
+}
+
+
